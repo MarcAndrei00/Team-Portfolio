6a8e320148067b2a8ac86442578c51fc89e3163e
Illyasviel-Von-Einzbern <fidelisalen@email.com>
2025-04-09 23:07:35 +0800

    new header

--- Team Portfolio/src/components/Navbar/Navbar.jsx
+++ Team Portfolio/src/components/Navbar/Navbar.jsx
@@ -2,33 +2,36 @@ import React, {useState} from "react";
 
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
+import logo from "../../../public/logo.png";
+
 
 export const Navbar = () => {
     return (
         <nav className={styles.navbar}>
+            <img src={logo} alt="Logo" style={{ height: "50px" }} />
             <a className={styles.title} href="/">
-                Team Portfolio
+                TFVON TECH SOLUTIONS
             </a>
-            <div className={styles.menu}>   
+            <div className={styles.menu}> 
             <img 
                 className={styles.menuBtn} 
                 src={getImageUrl("nav/menuIcon.png")} 
                 alt="menu-button"/>
                 <ul className={styles.menuItems}>
                     <li>
-                        <a href="#homepage">Homepage</a>
+                        <a href="#homepage">Home</a>
                     </li>
                     <li>
                         <a href="#about">About</a>
                     </li>
                     <li>
-                        <a href="#contact">Contact</a>
+                        <a href="#contact">Team</a>
                     </li>
                     <li>
                         <a href="#project">Project</a>
                     </li>
                     <li>
-                        <a href="#team">Team</a>
+                        <a href="#team">Contact</a>
                     </li>
                 </ul>
             </div>
